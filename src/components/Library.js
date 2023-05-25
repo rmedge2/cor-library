aff731b886e439c7a3fb08ece8181d9e7ce5c4ef
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-05-25 00:21:09 -0400

    most recent updates and changes

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,28 +2,49 @@ import { useState } from "react"
 import Shelf from "./Shelf"
 
 const Library = () => {
-    const [shelves, setShelves] = useState(
-        [
-            {
-                genre: 'Fiction',
-                books: [
-                    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-                    { title: '1984', author: 'George Orwell' },
-                ],
-            },
-            {
-                genre: 'Non-Fiction',
-                books: [
-                    { title: 'Sapiens', author: 'Yuval Noah Harari' },
-                    { title: 'Homo Deus', author: 'Yuval Noah Harari' },
-                ],
-            },
-        ])
+    const [shelves, setShelves] = useState([
+        {
+            genre: 'Fiction',
+            books: [
+                { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+                { title: '1984', author: 'George Orwell' },
+                { title: 'Beloved', author: 'Toni Morrison' },
+                { title: 'The Color Purple', author: 'Alice Walker' },
+            ],
+        },
+        {
+            genre: 'Mystery',
+            books: [
+                { title: 'Gone Girl', author: 'Gillian Flynn' },
+                { title: 'The Underground Railroad', author: 'Colson Whitehead' },
+                { title: 'IQ', author: 'Joe Ide' },
+                { title: 'Blacktop Wasteland', author: 'S. A. Cosby' },
+            ],
+        },
+        {
+            genre: 'Science Fiction',
+            books: [
+                { title: 'Kindred', author: 'Octavia Butler' },
+                { title: 'Dawn', author: 'Octavia Butler' },
+                { title: 'Parable of the Sower', author: 'Octavia Butler' },
+                { title: 'Binti', author: 'Nnedi Okorafor' },
+            ],
+        },
+        {
+            genre: 'Fantasy',
+            books: [
+                { title: 'Children of Blood and Bone', author: 'Tomi Adeyemi' },
+                { title: 'Akata Witch', author: 'Nnedi Okorafor' },
+                { title: 'Black Leopard, Red Wolf', author: 'Marlon James' },
+                { title: 'The Fifth Season', author: 'N. K. Jemisin' },
+            ],
+        },
+    ])
 
     return (
         <section>
             {
-                shelves.map(shelf => <Shelf genre={shelf.genre} books={shelf.books}/>)
+                shelves.map(shelf => <Shelf genre={shelf.genre} books={shelf.books} />)
             }
         </section>
     )
